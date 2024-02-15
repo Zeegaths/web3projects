7de2c9f68cd714a727e7b81b27ffa1ca24c1b616
Zeegaths <zarahgathoni76@gmail.com>
2024-02-15 14:46:43 +0100

    test

--- saveEther/test/saveTest.ts
+++ saveEther/test/saveTest.ts
@@ -32,4 +32,32 @@ describe("SaveEther", function () {
     expect(ownerSavings).to.equal(depositAmount);
     expect(depositAmount).to.equal(contractBalance);
   });
+
+  it("withdraw amount from savings.", async function() {
+    const depositAmount = ethers.parseEther("1.0");   
+
+    const txDeposit = await saveEther.deposit({value: depositAmount}); 
+
+    const receipt = await txDeposit.wait();
+    const { status } = receipt;
+    
+    const ownerSavingsBefore = await saveEther.checkSavings(await owner.getAddress());
+    const contractBalanceBefore = await saveEther.checkContractBal();
+
+    const withdrawTx = await saveEther.withdraw();
+    const withdrawReceipt = await withdrawTx.wait();
+    const withdrawStatus = withdrawReceipt.status;
+
+    const ownerSavingsAfter = await saveEther.checkSavings(await owner.getAddress());
+    const contractBalanceAfter = await saveEther.checkContractBal();
+
+    expect(status).to.equal(1);
+    expect(ownerSavingsBefore).to.equal(depositAmount);
+    expect(contractBalanceBefore).to.equal(depositAmount);
+
+    expect(withdrawStatus).to.equal(1); // Check withdrawal transaction status
+    expect(ownerSavingsAfter).to.equal(0); // Owner savings should be zero after withdrawal
+    expect(contractBalanceAfter).to.equal(0); // Contract balance should be zero after withdrawal
+});
+
 });
