4815785dfd8ab4858a7f12a51a5a646db62f495e
Zeegaths <zarahgathoni76@gmail.com>
2024-02-15 15:11:30 +0100

    savings test

--- saveEther/test/saveTest.ts
+++ saveEther/test/saveTest.ts
@@ -60,4 +60,34 @@ describe("SaveEther", function () {
     expect(contractBalanceAfter).to.equal(0); // Contract balance should be zero after withdrawal
 });
 
+it("send from savings.", async function() {
+  const depositAmount = ethers.parseEther("2.0"); 
+  const sendAmount = ethers.parseEther("1.0"); 
+
+  const txDeposit = await saveEther.deposit({ value: depositAmount }); 
+
+  const receipt = await txDeposit.wait();
+  const { status } = receipt;
+  
+  const ownerSavingsBefore = await saveEther.checkSavings(await owner.getAddress());
+  const contractBalanceBefore = await saveEther.checkContractBal();
+
+  // Provide the required parameters for sendOutSaving
+  const sendTx = await saveEther.sendOutSaving(await addr1.getAddress(), sendAmount);
+  const sendReceipt = await sendTx.wait();
+  const sendStatus = sendReceipt.status;
+
+  const ownerSavingsAfter = await saveEther.checkSavings(await owner.getAddress());
+  const contractBalanceAfter = await saveEther.checkContractBal();
+
+  expect(status).to.equal(1);
+  expect(ownerSavingsBefore).to.equal(depositAmount);
+  expect(contractBalanceBefore).to.equal(depositAmount);
+
+  expect(sendStatus).to.equal(1); // Check send transaction status
+  expect(ownerSavingsAfter).to.equal(depositAmount - sendAmount); // Owner savings should be reduced after sending
+  expect(contractBalanceAfter).to.equal(depositAmount - sendAmount); // Contract balance should be reduced after sending
+});
+
+
 });
